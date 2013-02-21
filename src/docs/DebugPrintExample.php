a94ec826a22dd0d831b8930b6474b95828e60b81
Florian Wolters <wolters.fl@gmail.com>
2013-02-21 17:15:44 +0100

    Updated example file. Signed-off-by:Florian Wolters <wolters.fl@gmail.com>

--- src/docs/DebugPrintExample.php
+++ src/docs/DebugPrintExample.php
@@ -1,43 +1,83 @@
 <?php
-use FlorianWolters\Component\Core\DebugPrintInterface;
+namespace FlorianWolters\Component\Core;
 
-require '../../vendor/autoload.php';
-require 'DebugPrintDefaultImpl.php';
-require 'DebugPrintCustomImpl.php';
+use FlorianWolters\Mock\DebugPrintCustomImpl;
+use FlorianWolters\Mock\DebugPrintDefaultImpl;
 
-exit(main());
+require __DIR__ . '/../../vendor/autoload.php';
 
-function main()
+/**
+ * The class {@see DebugPrintExample} implements a simple command line
+ * application to demonstrate the component
+ * **FlorianWolters\Component\Core\DebugPrint**.
+ *
+ * @author    Florian Wolters <wolters.fl@gmail.com>
+ * @copyright 2012-2013 Florian Wolters
+ * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
+ * @link      http://github.com/FlorianWolters/PHP-Component-Core-DebugPrint
+ * @since     Class available since Release 0.1.1
+ */
+final class DebugPrintExample
 {
-    demonstrateDefaultImplementation();
-    demonstrateCustomImplementation();
+    /**
+     * @param integer $argc
+     * @param array   $argv
+     *
+     * @return integer
+     */
+    public static function main($argc, array $argv = array())
+    {
+        $self = new self;
+        $self->demonstrateDefaultImplementation();
+        $self->demonstrateCustomImplementation();
+
+        return 0;
+    }
+
+    /**
+     * @return void
+     */
+    private function demonstrateDefaultImplementation()
+    {
+        $first = new DebugPrintDefaultImpl;
+        $second = new DebugPrintDefaultImpl;
+        $this->output($first);
+        $this->output($second);
+    }
+
+    /**
+     * @return void
+     */
+    private function demonstrateCustomImplementation()
+    {
+        $first = new DebugPrintCustomImpl('foo');
+        $second = new DebugPrintCustomImpl('bar');
+        $this->output($first);
+        $this->output($second);
+    }
+
+    /**
+     * @param DebugPrintInterface $obj
+     *
+     * @return void
+     */
+    private function output(DebugPrintInterface $obj)
+    {
+        // Explicit call of the Debug Print Method __toString().
+        $this->writeLine($obj->__toString());
+        // Implicit call of the Debug Print Method __toString().
+        $this->writeLine($obj);
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return void
+     */
+    private function writeLine($text = '')
+    {
+        echo $text . \PHP_EOL;
+    }
 }
 
-function demonstrateDefaultImplementation()
-{
-    $first = new DebugPrintDefaultImpl;
-    $second = new DebugPrintDefaultImpl;
-    output($first);
-    output($second);
-}
-
-function demonstrateCustomImplementation()
-{
-    $first = new DebugPrintCustomImpl('foo');
-    $second = new DebugPrintCustomImpl('bar');
-    output($first);
-    output($second);
-}
-
-function output(DebugPrintInterface $obj)
-{
-    // Explicit call of the Debug Print Method __toString().
-    writeLine($obj->__toString());
-    // Implicit call of the Debug Print Method __toString().
-    writeLine($obj);
-}
-
-function writeLine($text = '')
-{
-    echo $text . \PHP_EOL;
-}
+exit(DebugPrintExample::main($argc, $argv));
