4cd7f151587147fdfcf4b1c3f9b4f7b9de99c3ab
Florian Wolters <wolters.fl@gmail.com>
2013-02-21 17:12:06 +0100

    Replaced inline @link tags with @see and updated copyright year. Signed-off-by:Florian Wolters <wolters.fl@gmail.com>

--- src/php/FlorianWolters/Component/Core/DebugPrintInterface.php
+++ src/php/FlorianWolters/Component/Core/DebugPrintInterface.php
@@ -2,14 +2,14 @@
 namespace FlorianWolters\Component\Core;
 
 /**
- * A class implements the {@link DebugPrintInterface} interface to indicate that
+ * A class implements the {@see DebugPrintInterface} interface to indicate that
  * it offers a *Debug Print Method* with the name `__toString()`.
  *
  * This interface signals that the implementing class uses the *Debug Print
  * Method* implementation pattern.
  *
  * @author    Florian Wolters <wolters.fl@gmail.com>
- * @copyright 2012 Florian Wolters
+ * @copyright 2012-2013 Florian Wolters
  * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
  * @link      http://github.com/FlorianWolters/PHP-Component-Core-DebugPrint
  * @since     Interface available since Release 0.1.0
@@ -19,11 +19,11 @@ interface DebugPrintInterface
     /**
      * Returns a string representation of an object.
      *
-     * In general, the {@link __toString} method returns a string that
-     * "textually represents" an object. The result should be a concise but
-     * informative representation that is easy for a person to read.
+     * In general, the {@see __toString} method returns a string that "textually
+     * represents" an object. The result should be a concise but informative
+     * representation that is easy for a person to read.
      *
-     * The method {@link __toString} is intended to be used only for debugging
+     * The method {@see __toString} is intended to be used only for debugging
      * purposes, and the content and format of the returned string may vary
      * between implementations.
      *
--- src/php/FlorianWolters/Component/Core/DebugPrintTrait.php
+++ src/php/FlorianWolters/Component/Core/DebugPrintTrait.php
@@ -2,10 +2,10 @@
 namespace FlorianWolters\Component\Core;
 
 /**
- * The trait {@link DebugPrintTrait} provides a *Debug Print Method* for objects.
+ * The trait {@see DebugPrintTrait} provides a *Debug Print Method* for objects.
  *
  * @author    Florian Wolters <wolters.fl@gmail.com>
- * @copyright 2012 Florian Wolters
+ * @copyright 2012-2013 Florian Wolters
  * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
  * @link      http://github.com/FlorianWolters/PHP-Component-Core-DebugPrint
  * @since     Trait available since Release 0.1.0
@@ -17,12 +17,12 @@ trait DebugPrintTrait
     /**
      * Returns a string representation of the object.
      *
-     * In general, the {@link __toString} method returns a string that
-     * "textually represents" the object using the trait {@link
-     * DebugPrintTrait}. The result should be a concise but informative
-     * representation that is easy for a person to read.
+     * In general, the {@see __toString} method returns a string that "textually
+     * represents" the object using the trait {@see DebugPrintTrait}. The result
+     * should be a concise but informative representation that is easy for a
+     * person to read.
      *
-     * The {@link __toString} method for trait {@link DebugPrintTrait} returns a
+     * The {@see __toString} method for trait {@see DebugPrintTrait} returns a
      * string consisting of the name of the class of which the object is an
      * instance, the at-sign character `@`, and the unsigned hexadecimal
      * representation of the hash code of the object. In other words, this
@@ -31,11 +31,11 @@ trait DebugPrintTrait
      * \get_class($this) . '@' . $this->hashCode())
      * \---
      *
-     * The method {@link __toString} is intended to be used only for debugging
+     * The method {@see __toString} is intended to be used only for debugging
      * purposes, and the content and format of the returned string may vary
      * between implementations.
      *
-     * The method {@link __toString} implements the *Debug Print Method*
+     * The method {@see __toString} implements the *Debug Print Method*
      * implementation pattern.
      *
      * @return string The string representation.
